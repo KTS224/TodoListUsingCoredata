813bb5d171c7c62b0500ba5093667334c833343a
Taesung Kim <ts4683@gmail.com>
2022-08-09 03:34:37 +0900

    error 수정:

---
+++ TodoListCoredata/TodoListCoredata/Managers/CoreDataManager.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataManager.swift
+//  TodoListCoredata
+//
+//  Created by 김태성 on 2022/08/08.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    let persistentContainer: NSPersistentContainer
+    static let shared: CoreDataManager = CoreDataManager()
+    
+    private init() {
+        
+        persistentContainer = NSPersistentContainer(name: "SimpleTodoModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to initialize Core Data \(error)")
+            }
+        }
+        
+    }
+}
